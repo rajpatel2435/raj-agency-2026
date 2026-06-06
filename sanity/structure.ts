bf0e94282a6c9299b7da5c373a81544ff5d27653
rajpatel <rajpatel2435@gmail.com>
2026-06-06 11:53:39 -0700

    feat: new build errors

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,7 @@
-import type {StructureResolver} from 'sanity/structure'
-
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) =>
+export const structure = (S: any) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      ...S.documentTypeListItems()
+    ]);
